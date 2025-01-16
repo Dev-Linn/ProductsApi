88da8aac718b51c2b692a27869897386286190e0
Lincoln <linnsilva3636@gmail.com>
2025-01-15 23:36:53 -0300

    postProdutoH2

--- src/main/java/com/example/produtosApi/controller/ProdutoController.java
+++ src/main/java/com/example/produtosApi/controller/ProdutoController.java
@@ -2,19 +2,33 @@ package com.example.produtosApi.controller;
 
 
 import com.example.produtosApi.model.Produto;
+import com.example.produtosApi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Recebendo produto: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
+
 
 }
