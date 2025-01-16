3b5ea4d98653914a39110833a2a1854b70dbd815
Lincoln <linnsilva3636@gmail.com>
2025-01-15 23:23:38 -0300

    mapeamentoJPAEntityProduto

--- src/main/java/com/example/produtosApi/model/Produto.java
+++ src/main/java/com/example/produtosApi/model/Produto.java
@@ -1,19 +1,27 @@
 package com.example.produtosApi.model;
 
 
-
-import java.io.Serializable;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /// POJO > PLAIN OLD JAVA OBJECT
-public class Produto implements Serializable {
+@Entity
+@Table(name = "produto")
+public class Produto  {
 
 
-
+    @Id
+    @Column(name = "id")
     private String id;
+    @Column(name = "nome")
     private String nome;
+    @Column(name = "descricao")
     private String descricao;
+    @Column(name = "preco")
     private Double preco;
+
 
     public Produto() {
 
